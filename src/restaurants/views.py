97107f3c70b45dbf4af5220277f00509ec06b262
Mehnaz Singh <mehnaz.s.jaggi@hpe.com>
2019-05-12 16:43:12 -0700

    second commit. Templates working

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 def home(request):
-    html_var = "Hello World from f strings."    
-    html_ =f'''
-        <html>
-        <body>
-        <h1> {html_var.upper()} </h1>
-        </body>
-        </html>
-    '''
-    return HttpResponse(html_)
+    # html_var = "Hello World from f strings."    
+    # html_ =f'''
+    #     <html>
+    #     <body>
+    #     <h1> {html_var.upper()} </h1>
+    #     </body>
+    #     </html>
+    # '''
+    # return HttpResponse(html_)
+    num = random.randint(0,1000000)
+    return render(request,"base.html",{"randNum": num})
